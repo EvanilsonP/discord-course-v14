acb94d52cd047b128a1b52a152f70bc1864bd1ca
EvanilsonP <pevanilson99@gmail.com>
2023-05-27 16:07:36 -0300

    Slash Command Options and Choices

--- src/commands/register.js
+++ src/commands/register.js
@@ -1,7 +1,39 @@
+const { ApplicationCommandOptionType } = require('discord.js');
 const registerCommand = (
     {
-        name: 'hey',
-        description: 'Replies with hey'
+        name: 'add',
+        description: 'Replies with hey',
+        options: [
+            {
+                name: 'first-number',
+                description: 'The first number',
+                type: ApplicationCommandOptionType.Number,
+                choices: [
+                    {
+                        name: 'one',
+                        value: 1
+                    },
+
+                    {
+                        name: 'two',
+                        value: 2
+                    },
+
+                    {
+                        name: 'three',
+                        value: 3
+                    },
+                ],
+                required: true,
+            },
+
+            {
+                name: 'second-number',
+                description: 'The second number',
+                type: ApplicationCommandOptionType.Number,
+                required: true
+            },
+        ]
     }
 );
 
--- src/index.js
+++ src/index.js
@@ -33,8 +33,11 @@ main();
 client.on('interactionCreate', (interaction) => {
     if(!interaction.isChatInputCommand()) return;
 
-    if(interaction.commandName === 'hey') {
-        interaction.reply('Hey!');
+    if(interaction.commandName === 'add') {
+        const num1 = interaction.options.get('first-number').value;
+        const num2 = interaction.options.get('second-number').value;
+
+        interaction.reply(`The sum between ${num1} and ${num2} is ${num1 + num2}`);
     };
 });
 
