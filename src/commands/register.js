503d821fcfc0aec6ae796cde27e6e4d263e88991
EvanilsonP <pevanilson99@gmail.com>
2023-05-27 15:55:00 -0300

    Register and Run Slash Commands

---
+++ src/commands/register.js
@@ -0,0 +1,9 @@
+const registerCommand = (
+    {
+        name: 'hey',
+        description: 'Replies with hey'
+    }
+);
+
+module.exports = registerCommand;
+
--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 require('dotenv').config();
 
 const { Client, IntentsBitField } = require('discord.js');
+const { REST, Routes } = require('discord.js');
+
+const registerCommand = require('./commands/register');
 
 const client = new Client({
     intents: [
@@ -9,6 +12,30 @@ const client = new Client({
         IntentsBitField.Flags.GuildMessages,
         IntentsBitField.Flags.MessageContent
     ]
+});
+
+const rest = new REST({ version: '10'}).setToken(process.env.TOKEN);
+
+async function main() {
+    const commands = [registerCommand];
+
+    try {
+        console.log('Started refreshing application (/) commands.');
+        await rest.put(Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.GUILD_ID), {
+            body: commands
+        })
+    } catch (err) {
+        console.log(err);
+    }
+};
+main();
+
+client.on('interactionCreate', (interaction) => {
+    if(!interaction.isChatInputCommand()) return;
+
+    if(interaction.commandName === 'hey') {
+        interaction.reply('Hey!');
+    };
 });
 
 client.on('messageCreate', (message) => {
