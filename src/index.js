c50e287a9cdeb37e074ccbf3e2ab60ce9cc9ead8
EvanilsonP <pevanilson99@gmail.com>
2023-06-06 12:52:22 -0300

    Connecting to a Database

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 
 const { Client, IntentsBitField, EmbedBuilder, REST, Routes, ActionRowBuilder, ButtonBuilder, ButtonStyle, ActivityType } = require('discord.js');
 const command = require('./commands/register');
-
+const mongoose = require('mongoose');
 
 const client = new Client({
     intents: [
@@ -37,9 +37,17 @@ client.on('messageCreate', (message) => {
     }
 });
 
-client.on('ready', (c) => {
-    console.log(`${c.user.tag} is now online.`);
-});
 
-
+(async () => {
+    try {
+        mongoose.set('strictQuery', false);
+        await mongoose.connect('mongodb+srv://EvanilsonP:<88813091a>@cluster0.rq4v2yp.mongodb.net/?retryWrites=true&w=majority')
+        console.log('Connected to DB');
+    } catch (error) {
+        console.log(error);
+    }
+    client.on('ready', (c) => {
+        console.log(`${c.user.tag} is now online.`);
+    });
+})();
 client.login(process.env.TOKEN);
